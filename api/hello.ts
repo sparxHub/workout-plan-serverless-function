f8cf718620b46d9942879ff65ff2ecaeb708f380
Nadav Daniel <nadav@appv.io>
2024-01-16 12:51:05 +0200

    add a test

--- api/hello.ts
+++ api/hello.ts
@@ -13,24 +13,29 @@ import Handlebars from 'handlebars';
 import fetch from 'node-fetch'; // Import fetch for HTTP requests
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  if (req.method === 'POST') {
-    try {
-      const parsedBody = JSON.parse(req.body);
+	if (req.method === 'POST') {
+		try {
+			const parsedBody = JSON.parse(req.body);
 
-      // Fetch template from Vercel Blob storage
-      const templateUrl = 'https://jbuwdk1d7uenrxyq.public.blob.vercel-storage.com/html/template-8HW0p7Tmf8iriqnEeuUMZ3RGD0vteL.html';
-      const templateResponse = await fetch(templateUrl);
-      const templateText = await templateResponse.text();
+			// Fetch template from Vercel Blob storage
+			const templateUrl = 'https://jbuwdk1d7uenrxyq.public.blob.vercel-storage.com/html/template-8HW0p7Tmf8iriqnEeuUMZ3RGD0vteL.html';
+			const templateResponse = await fetch(templateUrl);
+			const templateText = await templateResponse.text();
 
-      const template = Handlebars.compile(templateText);
-      const renderedHtml = template(parsedBody);
+			const template = Handlebars.compile(templateText);
+			const renderedHtml = template(parsedBody);
 
-      res.status(200).send(renderedHtml);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error processing request');
-    }
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
+			res.status(200).send(renderedHtml);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send('Error processing request');
+		}
+	} else if (req.method === 'GET') {
+		const templateUrl = 'https://jbuwdk1d7uenrxyq.public.blob.vercel-storage.com/html/template-8HW0p7Tmf8iriqnEeuUMZ3RGD0vteL.html';
+		const templateResponse = await fetch(templateUrl);
+		res.status(200).send(templateResponse);
+
+	} else {
+		res.status(405).send('Method Not Allowed');
+	}
 }
