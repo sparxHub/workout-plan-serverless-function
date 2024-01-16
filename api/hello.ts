5afec7f4f0ab9e7d9b72550d0c53e33100f3d791
Nadav Daniel <nadav@appv.io>
2024-01-16 12:44:14 +0200

    init

--- api/hello.ts
+++ api/hello.ts
@@ -1,8 +1,36 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+// import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  const { name = 'World' } = req.query
-  return res.json({
-    message: `Hello ${name}!`,
-  })
+// export default function handler(req: VercelRequest, res: VercelResponse) {
+//   const { name = 'World' } = req.query
+//   return res.json({
+//     message: `Hello ${name}!`,
+//   })
+// }
+
+
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import Handlebars from 'handlebars';
+import fetch from 'node-fetch'; // Import fetch for HTTP requests
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (req.method === 'POST') {
+    try {
+      const parsedBody = JSON.parse(req.body);
+
+      // Fetch template from Vercel Blob storage
+      const templateUrl = 'https://jbuwdk1d7uenrxyq.public.blob.vercel-storage.com/html/template-8HW0p7Tmf8iriqnEeuUMZ3RGD0vteL.html';
+      const templateResponse = await fetch(templateUrl);
+      const templateText = await templateResponse.text();
+
+      const template = Handlebars.compile(templateText);
+      const renderedHtml = template(parsedBody);
+
+      res.status(200).send(renderedHtml);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error processing request');
+    }
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
 }
