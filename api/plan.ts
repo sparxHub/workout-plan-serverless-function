c22e4dfb5fc87e569714120afbd93d8c559c2ac7
Nadav Daniel <nadav@appv.io>
2024-01-21 23:43:02 +0200

    hello > plan

--- api/plan.ts
+++ api/plan.ts
@@ -14,7 +14,7 @@ import fetch from 'node-fetch'; // Import fetch for HTTP requests
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
 	if (req.method === 'GET') {
-		const templateUrl = 'https://jbuwdk1d7uenrxyq.public.blob.vercel-storage.com/html/template-8HW0p7Tmf8iriqnEeuUMZ3RGD0vteL.html';
+		const templateUrl = 'https://jbuwdk1d7uenrxyq.public.blob.vercel-storage.com/html/weekly_plan.html';
 		const templateResponse = await fetch(templateUrl);
 		res.status(200).send(await templateResponse.text());
 	} else {
